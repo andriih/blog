a491108ca095aa95cc67b1865ab68852a59fad8f
andriih <andrii.hnatyshyn@gmail.com>
2016-09-04 10:30:50 +0300

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<div class="header">Header</div>
+<div class="sidebar">Sidebar</div>
+<div class="main">Main</div>
+<div class="footer">Footer</div>
+
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 01.09.2016
+ * Time: 21:57
+ */
