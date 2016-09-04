b11b891018fd9f310ddf65f896a230fed38aa1e1
andriih <andrii.hnatyshyn@gmail.com>
2016-09-04 11:32:02 +0300

    autorization added

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-</head>
-<body>
-<div class="header">Header</div>
-<div class="sidebar">Sidebar</div>
-<div class="main">Main</div>
-<div class="footer">Footer</div>
-
-</body>
-</html>
-
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 01.09.2016
- * Time: 21:57
- */
+require __DIR__.'/autoload.php';
+
+$db = new \App\Db();
